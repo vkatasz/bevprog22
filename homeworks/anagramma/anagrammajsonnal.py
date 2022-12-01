6351823ee30373e3610f9150a7fd2a0affd7241d
vkatasz <varga.kata@atakv.hu>
2022-12-01 21:28:32 +0100

    anagramma hw

--- homeworks/anagramma/anagrammajsonnal.py
+++ homeworks/anagramma/anagrammajsonnal.py
@@ -1,14 +1,18 @@
 import json
 
 def main():
-    ops=open("anagrammak.json",'r')
-    sablon=json.load(ops)
-    anagrammae=input("adj meg egy stringet: ")
+    try:
+        ops=open("anagrammak.json",'r')
+        sablon=json.load(ops)
+        anagrammae=input("adj meg egy stringet: ")
 
-    if sablon[anagrammae]:
-        print(sablon[anagrammae])
-    else:
-        print("nem anagramma :(")
+        if sablon[anagrammae]:
+            print(sablon[anagrammae])
+        else:
+            pass
+        
+    except KeyError:
+        print("engedd el...")
 
 if __name__=="__main__":
     main()
